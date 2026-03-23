de1172af0813afe2deb77d60d17fb80dc04d9ac5
roomii13 <71903262+roomii13@users.noreply.github.com>
2026-03-23 17:34:31 -0300

    predictor

--- src/services/predictiveService.ts
+++ src/services/predictiveService.ts
@@ -1,1 +1,11 @@
+export async function getPrediccionRiesgo(data: any) {
+  const response = await fetch('http://localhost:8000/predict', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  });
+
+  if (!response.ok) throw new Error('Error en predicción');
+  return response.json();
+}
 
